0a6e826697f433707e7925793218415ac1715d4b
Pawel Mrowczynski <pawel.mrowczynski@smtsoftware.com>
2016-05-17 12:41:18 +0200

    Add project files.

---
+++ build.ps1
@@ -0,0 +1,23 @@
+﻿cls
+
+# '[p]sake' is the same as 'psake' but $Error is not polluted
+Remove-Module [p]sake
+
+$psakeModule = (Get-ChildItem (".\packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -Last 1
+
+Import-Module $psakeModule
+
+# You can put arguments to task in multiple lines using
+Invoke-psake -buildFile .\Build\default.ps1 `
+				-taskList Test `
+				-framework 4.5.2 `
+				-properties @{
+					"buildConfiguration" = "Debug"
+					"buildPlatform" = "Any CPU"
+					"testMessage" = "What am I doing"} `
+				-parameters @{"solutionFile" = "..\psake.sln"}
+
+Write-Host "Build exit code: " $LastExitCode
+
+#Propagating the exit code so that builds actually fail when there is a problem
+exit $LastExitCode			
