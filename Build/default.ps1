d60e4df3a1a528383dcf041449e17baf3b6ac69b
Pawel Mrowczynski <pawel.mrowczynski@smtsoftware.com>
2016-05-27 14:16:21 +0200

    Added code coverage metrics with use of OpenCover and ReportGenerator

--- Build/default.ps1
+++ Build/default.ps1
@@ -3,8 +3,9 @@ properties {
 	$cleanMessage = "Cleaned!"
 	$compileMessage = "Compile done"
 	$testMessage = "Tests done!"
+
 	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
-	$outputDirectory = "..\.build"
+	$outputDirectory= "$solutionDirectory\.build"
 	$temporaryOutputDirectory = "$outputDirectory\temp"
 
 	$publishedNUnitTestsDirectory = "$temporaryOutputDirectory\_PublishedNUnitTests"
@@ -18,8 +19,11 @@ properties {
 	$xunitTestResultsDirectory = "$testResultsDirectory\xUnit"
 	$MSTestTestResultsDirectory = "$testResultsDirectory\MSTest"
 
-
-
+	$testCoverageDirectory = "$outputDirectory\TestCoverage"
+	$testCoverageReportPath = "$testCoverageDirectory\OpenCover.xml"
+	$testCoverageFilter = "+[*]* -[xunit.*]* -[*.NUnitTests]* -[*.Tests]* -[*.xUnitTests]*"
+	$testCoverageExcludeByAttribute = "*.ExcludeFromCodeCoverage*"
+	$testCoverageExcludeByFile = "*\*Designer.cs;*\*.g.cs;*\*.g.i.cs"
 
 	$buildConfiguration = "Release"
 	$buildPlatform = "Any CPU"
@@ -29,6 +33,11 @@ properties {
 	$NUnitExe = (Find-PackagePath $packagesPath "Nunit.Runners" )  + "\tools\nunit-console-x86.exe"
 	$xUnitExe = (Find-PackagePath $packagesPath "xunit.runner.console" )  + "\tools\xunit.console.exe"
 	$vsTestExe = (Get-ChildItem ("C:\Program Files (x86)\Microsoft Visual Studio*\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe")).FullName | Sort-Object $_ | Select -Last 1
+	
+	$openCoverExe = (Find-PackagePath $packagesPath "OpenCover") + "\Tools\OpenCover.Console.exe"
+		$reportGeneratorExe = (Find-PackagePath $packagesPath "ReportGenerator") + "\Tools\ReportGenerator.exe"
+
+
 	}  
 
 
@@ -50,6 +59,10 @@ task Init -description "Initialises the build by removing previous artifacts and
 	Assert (Test-Path $NUnitExe) "NUnit Console could not be found"
 	Assert (Test-Path $xUnitExe) "xUnit Console could not be found"
 	Assert (Test-Path $vsTestExe) "vsTest Console could not be found"
+
+	Assert (Test-Path $openCoverExe) "OpenCover Console could not be found" 
+	Assert (Test-Path $reportGeneratorExe) "ReportGenerator Console could not be found"
+
 
 	# Remove previous build results
 	if (Test-Path $outputDirectory)
@@ -90,12 +103,20 @@ task TestNunit `
 
 		$testAssemblies = Prepare-Tests -testRunnerName "NUnit" `
 										-publishedTestsDirectory $publishedNUnitTestsDirectory `
-										-testResultsDirectory $NUnitTestResultsDirectory
+										-testResultsDirectory $NUnitTestResultsDirectory `
+										-testCoverageDirectory $testCoverageDirectory
 
-		Exec { &$NUnitExe $testAssemblies /xml:$NUnitTestResultsDirectory\NUnit.xml /nologo /noshadow}
+		$targetArgs = "$testAssemblies /xml:`"`"$NUnitTestResultsDirectory\NUnit.xml`"`" /nologo /noshadow"
 
 		Write-Host  $NUnitTestResultsDirectory
-		
+		#Run Open Cover, that will also run Tests
+		Run-Tests -openCoverExe $openCoverExe `
+					-targetExe $NUnitExe `
+					-targetargs $targetArgs `
+					-coveragePath $testCoverageReportPath `
+					-filter $testCoverageFilter  `
+					-excludebyattribute: $testCoverageExcludeByAttribute `
+					-excludebyfile:$testCoverageExcludeByFile
 	}
 
 task TestXunit `
@@ -105,11 +126,20 @@ task TestXunit `
 	{
 		$testAssemblies = Prepare-Tests -testRunnerName "xUnit" `
 										-publishedTestsDirectory $publishedxUnitTestsDirectory `
-										-testResultsDirectory $xUnitTestResultsDirectory
+										-testResultsDirectory $xUnitTestResultsDirectory `
+										-testCoverageDirectory $testCoverageDirectory
 
-		Exec { &$xUnitExe $testAssemblies -xml $xUnitTestResultsDirectory\xUnit.xml -nologo -noshadow}
 
-		Write-Host  $xUnitTestResultsDirectory
+		$targetArgs = "$testAssemblies -xml `"`"$xUnitTestResultsDirectory\xUnit.xml`"`" -nologo -noshadow"
+
+		#Run Open Cover, that will also run Tests
+		Run-Tests -openCoverExe $openCoverExe `
+					-targetExe $xUnitExe `
+					-targetargs $targetArgs `
+					-coveragePath $testCoverageReportPath `
+					-filter $testCoverageFilter  `
+					-excludebyattribute: $testCoverageExcludeByAttribute `
+					-excludebyfile:$testCoverageExcludeByFile
 	}
 task TestMSTest `
 	-depends Compile `
@@ -118,14 +148,27 @@ task TestMSTest `
 	{
 		$testAssemblies = Prepare-Tests -testRunnerName "MSTest" `
 										-publishedTestsDirectory $publishedMSTestTestsDirectory `
-										-testResultsDirectory $MSTestTestResultsDirectory
+										-testResultsDirectory $MSTestTestResultsDirectory `
+										-testCoverageDirectory $testCoverageDirectory
 
 		#vs test doesn't have any optio to change the output directory so we need to change working dir for a while
 		Push-Location $MSTestTestResultsDirectory
-		Exec {&$vsTestExe $testAssemblies /Logger:trx}
+
+
+		$targetArgs = "$testAssemblies /Logger:trx"
+				#Run Open Cover, that will also run Tests
+		Run-Tests -openCoverExe $openCoverExe `
+					-targetExe $vsTestExe `
+					-targetargs $targetArgs `
+					-coveragePath $testCoverageReportPath `
+					-filter $testCoverageFilter  `
+					-excludebyattribute: $testCoverageExcludeByAttribute `
+					-excludebyfile:$testCoverageExcludeByFile
+
+
 		Pop-Location
 
-		#move the .trx file bak to $MSTestTestResultsDirectory
+		#move the .trx file back to $MSTestTestResultsDirectory
 		Move-Item -Path $MSTestTestResultsDirectory\TestResults\*.trx -Destination $MSTestTestResultsDirectory\MSTest.trx
 
 		Remove-Item $MSTestTestResultsDirectory\TestResults
@@ -136,5 +179,40 @@ task TestMSTest `
 task Test `
 	-depends Compile, TestNunit, TestXunit, TestMSTest `
 	-description "Run unit tests"{
-	Write-Host $testMessage
+	if (Test-Path $testCoverageReportPath)
+	{
+		# Generate HTML test coverage report
+		Write-Host "`r`nGenerating HTML test coverage report"
+		Exec { &$reportGeneratorExe $testCoverageReportPath $testCoverageDirectory }
+
+		Write-Host "Parsing open Cover results"
+		#Load the coverage report as XML
+		$coverage = [xml](Get-Content -Path $testCoverageReportPath)
+
+		$coverageSummary = $coverage.CoverageSession.Summary
+
+		# Write class coverage
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageAbsCCovered' value='$($coverageSummary.visitedClasses)']"
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageAbsCTotal' value='$($coverageSummary.numClasses)']"
+		Write-Host ("##teamcity[buildStatisticValue key='CodeCoverageC' value='{0:N2}']" -f (($coverageSummary.visitedClasses / $coverageSummary.numClasses)*100))
+
+		# Report method coverage
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageAbsMCovered' value='$($coverageSummary.visitedMethods)']"
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageAbsMTotal' value='$($coverageSummary.numMethods)']"
+		Write-Host ("##teamcity[buildStatisticValue key='CodeCoverageM' value='{0:N2}']" -f (($coverageSummary.visitedMethods / $coverageSummary.numMethods)*100))
+		
+		# Report branch coverage
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageAbsBCovered' value='$($coverageSummary.visitedBranchPoints)']"
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageAbsBTotal' value='$($coverageSummary.numBranchPoints)']"
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageB' value='$($coverageSummary.branchCoverage)']"
+
+		# Report statement coverage
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageAbsSCovered' value='$($coverageSummary.visitedSequencePoints)']"
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageAbsSTotal' value='$($coverageSummary.numSequencePoints)']"
+		Write-Host "##teamcity[buildStatisticValue key='CodeCoverageS' value='$($coverageSummary.sequenceCoverage)']"
+	}
+	else
+	{
+		Write-Host "No coverage file found at: $testCoverageReportPath"
+	}
 	}
